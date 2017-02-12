409d073a9022636fca89e001af6dd1f2af8df1c4
Jason Presley <jpresley@users.noreply.github.com>
2017-02-11 22:09:46 -0600

    Updated for stand alone functionality

--- LogsController.cs
+++ LogsController.cs
@@ -1,64 +1,61 @@
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Linq;
 using System.Net;
-using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
-using MVC2cStore.Data;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
 
-namespace MVC2cStore.Controllers
+namespace CreativityJungle
 {
+    public partial class Log
+    {
+        [Key]
+        public int ID { get; set; }
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public System.DateTime Date { get; set; }
+        public string Thread { get; set; }
+        public string Level { get; set; }
+        public string Logger { get; set; }
+        public string Message { get; set; }
+        public string Exception { get; set; }
+    }
+
     public class LogsController : Controller
     {
-        private UnitOfWork uow = new UnitOfWork();
+        string connstring = WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
         // GET: Logs
         public ActionResult Index()
         {
-            return View(uow.LogRepository.Get());
-        }
+            List<Log> logs = new List<Log>();
 
-        // GET: Logs/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
+            using (SqlConnection conn = new SqlConnection(connstring))
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Log log = uow.LogRepository.Find(id);
-            if (log == null)
-            {
-                return HttpNotFound();
-            }
-            return View(log);
-        }
+                conn.Open();
 
-        // GET: Logs/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Log log = uow.LogRepository.Find(id);
-            if (log == null)
-            {
-                return HttpNotFound();
-            }
-            return View(log);
-        }
+                SqlCommand cmdGetLogs = new SqlCommand("Select * from log", conn);
+                SqlDataReader rdrLogs = cmdGetLogs.ExecuteReader();
 
-        // POST: Logs/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Log log = uow.LogRepository.Find(id);
-            uow.LogRepository.Remove(log);
-            uow.Save();
-            return RedirectToAction("Index");
+                while (rdrLogs.Read())
+                {
+                    // Create a AuthorModel object
+                    Log log = new Log()
+                    {
+                        ID = (int)rdrLogs["ID"],
+                        Date = (DateTime)rdrLogs["Date"],
+                        Thread = (string)rdrLogs["Thread"],
+                        Level = (string)rdrLogs["Level"],
+                        Logger = (string)rdrLogs["Logger"],
+                        Message = (string)rdrLogs["Message"],
+                        Exception = (string)rdrLogs["Exception"]
+                    };
+                    logs.Add(log);
+                }
+                rdrLogs.Close();
+                conn.Close();
+            }
+            return View(logs);
         }
 
         // Methods for use with jQuery AJAX calls
@@ -67,11 +64,16 @@ namespace MVC2cStore.Controllers
         [AcceptVerbs(HttpVerbs.Post)]
         public JsonResult AjaxDelete(int id)
         {
-            Log log = uow.LogRepository.Find(id);
-            uow.LogRepository.Remove(log);
-            uow.Save();
+            using (SqlConnection conn = new SqlConnection(connstring))
+            {
+                conn.Open();
 
-            var message = string.Format("Deleted Log '{0}' from the database!", log.Message);
+                SqlCommand cmdGetLogs = new SqlCommand("delete from log where id = " + id, conn);
+                cmdGetLogs.ExecuteNonQuery();
+                conn.Close();
+            }
+
+            var message = string.Format("Log record deleted from the database!");
             return Json(new
             {
                 id = id,
@@ -83,11 +85,32 @@ namespace MVC2cStore.Controllers
         [HttpGet]
         public PartialViewResult LogDetails(int id)
         {
-            Log log = uow.LogRepository.Find(id);
+            Log log = null;
+
+            using (SqlConnection conn = new SqlConnection(connstring))
+            {
+                conn.Open();
+
+                SqlCommand cmdGetLogs = new SqlCommand("Select * from log where id = " + id, conn);
+                SqlDataReader rdrLog = cmdGetLogs.ExecuteReader();
+
+                while (rdrLog.Read())
+                {
+                    // Create a AuthorModel object
+                    log = new Log()
+                    {
+                        Date = (DateTime)rdrLog["Date"],
+                        Thread = (string)rdrLog["Thread"],
+                        Level = (string)rdrLog["Level"],
+                        Logger = (string)rdrLog["Logger"],
+                        Message = (string)rdrLog["Message"],
+                        Exception = (string)rdrLog["Exception"]
+                    };
+                }
+                rdrLog.Close();
+                conn.Close();
+            }
             return PartialView(log);
         }
-
-
-
     }
 }
